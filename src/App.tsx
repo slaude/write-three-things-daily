a6ea8fec9db1c3d7309afeee9edd262a0136327b
Sean Laude <sean.laude@braze.com>
2022-08-04 15:04:52 -0400

    some more refinements

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,14 @@ function App() {
   const [second, setSecond] = useState("");
   const [third, setThird] = useState("");
 
+  const [editState, setEditState] = useState<"editing" | "confirming" | "confirmed">("editing");
+
   const disableSecond = first.length < YOU_MUST_TYPE_THIS_MANY_CHARACTERS;
   const disableThird = disableSecond || second.length < YOU_MUST_TYPE_THIS_MANY_CHARACTERS;
-  const disableSumbit = disableSecond || disableThird || third.length < YOU_MUST_TYPE_THIS_MANY_CHARACTERS;
+  const disableSubmit = disableSecond || disableThird || third.length < YOU_MUST_TYPE_THIS_MANY_CHARACTERS;
 
   const handleSubmit = useCallback<FormEventHandler<HTMLFormElement>>((e) => {
+    setEditState("confirmed");
     const [firstValue, secondValue, thirdValue] = Array.from(e.target as any).map((n: any) => n.value);
 
     console.log("committed", firstValue, secondValue, thirdValue);
@@ -24,27 +27,81 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1 className="text-3xl m-4 font-bold text-center">Write exactly three things a day</h1>
+    <div className="App max-w-screen-lg mx-auto">
+      <h1 className="text-3xl m-4 font-bold text-center">
+        {editState === "confirmed" ? "The three things you wrote today:" : "Write exactly three things a day"}
+      </h1>
       <form className="container mx-auto flex flex-col items-center" onSubmit={handleSubmit}>
-        <div className="">
-          <Input name="first" value={first} onChange={setFirst} />
+        <div className="w-full">
+          <Input
+            placeholder="Write your first thing"
+            name="first"
+            value={first}
+            disabled={editState !== "editing"}
+            onChange={setFirst}
+          />
         </div>
-        <div>
-          <Input name="second" value={second} disabled={disableSecond} onChange={setSecond} />
+        <div className="w-full">
+          <Input
+            placeholder={disableSecond ? "" : "Next, write your second thing"}
+            name="second"
+            value={second}
+            disabled={disableSecond || editState !== "editing"}
+            onChange={setSecond}
+          />
         </div>
-        <div>
-          <Input name="third" value={third} disabled={disableThird} onChange={setThird} />
+        <div className="w-full">
+          <Input
+            placeholder={disableThird ? "" : "Finally, write your third thing"}
+            name="third"
+            value={third}
+            disabled={disableThird || editState !== "editing"}
+            onChange={setThird}
+          />
         </div>
-        <div>
-          <button
-            type="submit"
-            className={classNames("", { "text-slate-500": disableSumbit, "cursor-n-resize": disableSumbit })}
-            aria-label="Commit your writings"
-            disabled={disableSumbit}
-          >
-            That's it for today
-          </button>
+        <div className="w-full flex justify-between">
+          {editState === "editing" && (
+            <button
+              type="button"
+              className={
+                classNames(
+                  "text-center w-full rounded font-bold p-2 text-white bg-cyan-800",
+                  {
+                    "text-slate-500": disableSubmit,
+                    "cursor-n-resize": disableSubmit,
+                    "bg-slate-300": disableSubmit
+                  }
+                )
+              }
+              aria-label="Commit your writings"
+              disabled={disableSubmit}
+              onClick={(e) => {
+                setEditState("confirming")
+                e.preventDefault();
+              }}
+            >
+              {disableSubmit ? "You're not quite there": "That's it for today"}
+            </button>
+          )}
+          {editState === "confirming" && (
+            <>
+              <button
+                className="flex-1 mr-1 text-center rounded font-bold p-2 text-white bg-red-600"
+                onClick={(e) => {
+                  setEditState("editing")
+                  e.preventDefault()
+                }}
+              >
+                Make some edits
+              </button>
+              <button
+                className="flex-1 ml-1 text-center rounded font-bold p-2 text-white bg-green-600"
+                type="submit"
+              >
+                Submit my three things
+              </button>
+            </>
+          )}
         </div>
       </form>
     </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,11 +6,12 @@ interface InputProps extends Partial<HTMLTextAreaElement> {
   onChange: (value: string) => void;
 }
 
-export const Input = ({ name, value, disabled, onChange }: InputProps): React.ReactElement => {
+export const Input = ({ name, placeholder, value, disabled, onChange }: InputProps): React.ReactElement => {
   return (
     <textarea
       name={name}
-      className={classnames("border-2 p-2 resize-none", { "cursor-n-resize": disabled })}
+      placeholder={placeholder}
+      className={classnames("border-2 p-2 rounded resize-none w-full", { "cursor-n-resize": disabled })}
       disabled={disabled}
       value={value}
       onChange={(e) => onChange(e.target.value)}
